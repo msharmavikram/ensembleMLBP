961ef48f49058d3ca987f9cbb0cdbf00e2fc47c6
Umur <darbaz2@illinois.edu>
2017-12-14 20:29:19 -0600

    Final commit for EnsembleBP, good work all

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # ensembleMLBP
-EnsembleMLBP is a ECE511 course project created by Vikram, Umur and Andrew. Here we explore ensemble+ML based branch predictor. Initially we analyse tournamentBP and provide it loopholes. Then we evaluate weighted majority selection scheme for TournamentBP.
+EnsembleMLBP is a ECE511 course project created by Vikram, Umur and Andrew. Here we explore ensemble+ML based branch predictor. Initially we analyzed tournamentBP and its downsides. Then we evaluated weighted majority selection scheme in the Ensemble context.
 
-There are several commands to run different kinds of benchmarks. 
+There are several commands to run different kinds of benchmarks. We assume you have built it to support ARM64 OoO core and that the benchmarks will be running in SE mode.
 
 Command : 
- gem5/run.sh 
+ gem5/rungem5.sh soplex
+ gem5/run_microbench.sh
+ gem5/run_sebench.sh
+ gem5/run_all_benchmarks.sh
+ # To reproduce the results discussed in the report, use run_all_benchmarks.sh
 
-Benchmark: 
- compile : 
-     script/run.sh 
+To parse and generate data, scripts are provided under the scripts subdirectory. The benchmarks used are precompiled and stored as binaries under benchmark
+The EnsembleBP predictor should be selected in the ARM OoO config scripts. EnsembleBP2 was used as an experiment file and it is not discussed in our report.
+TriTournament is the 3-way discrete selection. The report is available in the doc folder. Final results are included for your reference under results.
+
+Please feel free to contact us regarding any questions.
 
--- script/extract_stats.py
+++ script/extract_stats.py
@@ -3,8 +3,18 @@ import os
 import re
 
 # Benchmarks to parse
-benchmarks = ["step1", "step2", "step3", "step4", "step5", "step6", "step7", "step8", "step9", "Bubblesort", "Oscar", "Perm", "Puzzle", "Quicksort"] #"Trees"
-phrases = ["condPredicted", "condIncorrect", "atLeastOneCorrectExpert", "allExpertsWrong", "allExpertsRight", "lowWeightExpertsWon"]
+benchmarks = ["step1", "step2", "step3", "step4", "step5", "step6", "step7", "Bubblesort", "Perm", "Puzzle", "Quicksort", "soplex"]
+phrases = ["condPredicted", "condIncorrect", "atLeastOneCorrectExpert", "oneCorrectExpert", "twoCorrectExpert" "allExpertsWrong", "allExpertsRight", "lowWeightExpertsWon"]
+
+predicted = 0
+mispredicted = 0
+atleastonecorrect = 0
+allwrong = 0
+allright = 0
+lowweightwon = 0
+majoritycorrect = 0
+onecorrectexpert = 0
+twocorrectexpert = 0
 
 # Directory with results
 directory = sys.argv[1]
@@ -13,8 +23,37 @@ for benchmark in benchmarks:
 	fileName = directory + "/" + benchmark + "/stats.txt"
 	statLog = open(fileName, "r")
 	for line in statLog:
-		for phrase in phrases:
-			if phrase in line:
-				words = line.split()
-				print "Benchmark: {}, Key: {}, Count: {}".format(benchmark, phrase, words[1])
+		words = line.split()
+		if "condPredicted" in line:
+			predicted = int(words[1])
+		elif "condIncorrect" in line:
+			mispredicted = int(words[1])
+		elif "atLeastOneCorrectExpert" in line:
+			atleastonecorrect = int(words[1])
+		elif "oneCorrectExpert" in line:
+			onecorrectexpert = int(words[1])
+		elif "twoCorrectExpert" in line:
+			twocorrectexpert = int(words[1])
+		elif "allExpertsWrong" in line:
+			allwrong = int(words[1])
+		elif "allExpertsRight" in line:
+			allright = int(words[1])
+		elif "lowWeightExpertsWon" in line:
+			lowweightwon = int(words[1])
+	print "Benchmark: {}".format(benchmark)
+	print "Accuracy: {}".format(100*float(mispredicted)/(mispredicted+predicted))
+	print "AtLeastOneCorrect: {}".format(100*float(atleastonecorrect)/(mispredicted))
+	print "MajorityCorrect: {}".format(100*float(twocorrectexpert)/(mispredicted))
+	print "AllExpertsWrong: {}".format(100*float(allwrong)/mispredicted)
+	print "UniformVote: {}".format(100*float(allwrong+allright)/(predicted+mispredicted))
+	print "LowWeightsWon: {}".format(100*float(lowweightwon)/(predicted+mispredicted))
+
+# for benchmark in benchmarks:
+# 	fileName = directory + "/" + benchmark + "/stats.txt"
+# 	statLog = open(fileName, "r")
+# 	for line in statLog:
+# 		for phrase in phrases:
+# 			if phrase in line:
+# 				words = line.split()
+# 				print "Benchmark: {}, Key: {}, Count: {}".format(benchmark, phrase, words[1])
 
